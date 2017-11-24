2a9a1c1d29059acf04dc9ae5729d7bf527a9cff2
Raghuvinayak Rao Medisetti <raghuvinayak99@gmail.com>
2017-11-24 18:47:47 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,32 @@
 
+import sys
+import socket
+import threading
+from flask import Flask
+from flask import send_file
+import os
+import time
+from multiprocessing import Process
+import subprocess, shlex
+
+
+app = Flask(__name__)
+
+
+
+#DPMI strings 
+@app.route('/run/<string:stream>', methods = ['GET'])
+def start_process(stream):
+        address = stream.split('_')
+        global gstream
+        gstream = '::'.join(address)
+        Process(target = start_king(stream)).start()
+        return "please wait........"
+
+#stop DPMI utils
+
+@app.route('/stop', methods=['GET'])
+def stop_service():
+         os.system("sudo pkill bitrate")
+#pkill bitrate
+         return "DPMI bitrate stoped\n"
