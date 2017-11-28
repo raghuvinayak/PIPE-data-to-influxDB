77b550aebca4bfc285da31fd135c84a16aa2ada3
Raghuvinayak Rao Medisetti <raghuvinayak99@gmail.com>
2017-11-28 11:53:56 +0100

    Update api.py

--- api.py
+++ api.py
@@ -30,3 +30,16 @@ def stop_service():
          os.system("sudo pkill bitrate")
 #pkill bitrate
          return "DPMI bitrate stoped\n"
+
+#add New stream
+
+@app.route('/add/<string:NewStream>', methods=['GET'])
+def add_stream(NewStream):
+
+        gstream=start_process(stream)
+	address = NewStream.split('_')
+	addstream = '::'.join(address)
+        stop_service()
+        time.sleep(1)
+        start_process(gstream+' '+addstream)
+        return "stream added\n"
